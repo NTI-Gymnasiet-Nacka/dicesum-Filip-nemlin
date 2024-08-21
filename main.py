a7d6b3a8c9a950e863526580cb1a534e73e121d9
Filip-nemlin <filip.nemlin@elev.ntig.se>
2024-08-21 11:11:06 +0200

    första uppgift

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
+# Författare: Filip Nemlin
+# Datum:2024-08-21
+
+from pdb import Restart
+
 
 def main():
-    user_input = input().split(" ")
+    user_input = input("skriv två nummer på hur många sidor tärningarna ska ha, mellan 4 och 20. Använd ett mellanslag mellan numren").split(" ")
+
+    dice1=str(user_input[0])
+    dice2=str(user_input[1])
+    sum=[]
+    for a in range (1, dice1+1):
+        for b in range (1, dice2+1):
+            if a+b in sum:
+                sum[a+b] +=1
+
+
+
+
 
 if __name__ == "__main__":
     main()
+
